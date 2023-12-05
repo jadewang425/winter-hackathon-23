616dede936a81bea1968db2838cff96e0e46d45c
mar69287 <marcoa.ruiz97@gmail.com>
2023-12-04 17:09:05 -0800

    create starter code for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import AdoptionPage from './pages/AdoptionPage'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
