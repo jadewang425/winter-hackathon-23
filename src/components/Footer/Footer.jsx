ce2d67916c1cb799170143b565f7f292c6ef8a45
jadewang425 <jadewang425@gmail.com>
2023-12-05 18:23:07 -0500

    add Facebook link to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ export default function Footer () {
                 <div className="hover:underline"><Link to="about">About Us</Link></div>
                 <div className="hover:underline"><Link to="/adoption">Adoption</Link></div>
                 <div className="hover:underline"><Link to="/contactus">Contact Us</Link></div>
+                <div className="hover:underline"><a href="https://www.facebook.com/barkanimalrescue"  target="_blank" rel="noreferrer">Facebook</a></div>
             </div>
             <div className="text-xs w-[50%] h-auto self-end mb-3">
                 <div className="inset-x-0 bottom-0 justify-end">Boudreaux’s Animal Rescue Krewe (B.A.R.K.) is a 501(c)(3) non-profit animal welfare organization.</div>
