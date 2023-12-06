000bbbe5ad2dee918a1ad78fe9ec0ea48e88364c
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-12-05 19:18:45 -0500

    test pet data

--- src/components/Navbar/AvailablePets.jsx
+++ src/components/Navbar/AvailablePets.jsx
@@ -1,72 +1,73 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
 import { usePet } from "../../utilities/PetContext"
 
 export default function AvailablePets() {
     const { getPetByType } = usePet();
-    const [dogs, setDogs] = useState([]);
-    const [cats, setCats] = useState([]);
-    const [error, setError] = useState("");
-    const organizationId = "la424"
+    // const [dogs, setDogs] = useState([]);
+    // const [cats, setCats] = useState([]);
+    // const [error, setError] = useState("");
+    const [pets, setPets] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-            setError("");
-            const dogData = await getPetByType("dog", organizationId);
-            setDogs(dogData.slice(0,30))
-            const catData = await getPetByType("cat", organizationId);
-            setCats(catData.slice(0,30))
-            } catch (error) {
-            console.error("Error fetching pets:", error);
-            setError("Error fetching pets. Please try again.");
-            }
-        };
-        fetchData();
-    }, [getPetByType, organizationId]);
+        async function getPets() {
+            
+            const petsByType = await getPetByType();
+            const pets = petsByType.slice(0,10)
+            setPets(pets)
+            console.log(pets)
+        }
+        getPets();
+    }, []);
+    // const filteredDogs = dogsData.filter((dog) => dog.organization_id === "la424");
 
-return (
-    <div className="w-full h-screen">
-        <h1>Find A Pet</h1>
-        <div className="flex gap-10 m-10 ">
+    // setDogs(filteredDogs.slice(0, 30));
+
+    // const catsData = await getPetByType(animalType);
+    // const filteredCats = catsData.filter((cat) => cat.organization_id === "la424");
+    // setCats(filteredCats.slice(0, 30));
+
+    return (
+        <div className="w-full h-screen">
+            <h1>Find A Pet</h1>
+            <div className="flex gap-10 m-10 ">
+            </div>
+            {/* <div>
+                <h2>Available Dogs</h2>
+                {dogs.map((dog) => (
+                    <div key={dog.id}>
+                        <img 
+                            src={dog.photos.length > 0 && dog.photos[0].small} 
+                            alt={dog.name} 
+                            style={{ maxWidth: '100px'}} 
+                        />
+                        <Link to={`/adoption/${dog.id}`}>{dog.name}</Link>
+                        <p>{dog.age}</p>
+                    </div>
+                    
+
+                ))}
+
+            </div>
+            
+            <div>
+                <h2>Available Cats</h2>
+                {cats.map((cat) => (
+                    <div key={cat.id}>
+                        <img 
+                            src={cat.photos.length > 0 && cat.photos[0].small} 
+                            alt={cat.name} 
+                            style={{ maxWidth: '100px'}} 
+                        />
+                        <Link to={`/adoption/${cat.id}`}>{cat.name}</Link>
+                        <p>{cat.age}</p>
+                    </div>
+                    
+
+                ))}
+
+            </div> */}
+            
         </div>
-        {error && <p className="text-red-500">{error}</p>}
-        <div>
-            <h2>Dogs</h2>
-            {dogs.map((dog) => (
-                <div key={dog.id}>
-                    <img 
-                        src={dog.photos.length > 0 && dog.photos[0].small} 
-                        alt={dog.name} 
-                        style={{ maxWidth: '100px'}} 
-                    />
-                    <Link to={`/adoption/${dog.id}`}>{dog.name}</Link>
-                    <p>{dog.age}</p>
-                </div>
-                
-
-            ))}
-
-        </div>
-        
-        <div>
-            <h2>Cats</h2>
-            {dogs.map((dog) => (
-                <div key={dog.id}>
-                    <img 
-                        src={dog.photos.length > 0 && dog.photos[0].small} 
-                        alt={dog.name} 
-                        style={{ maxWidth: '100px'}} 
-                    />
-                    <Link to={`/adoption/${dog.id}`}>{dog.name}</Link>
-                    <p>{dog.age}</p>
-                </div>
-                
-
-            ))}
-
-        </div>
-        
-    </div>
-    );
+        );
 }
