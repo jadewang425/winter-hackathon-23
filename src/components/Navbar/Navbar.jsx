babdf2ff56350ca32a14dbeb282ccd3c1a50637a
mar69287 <marcoa.ruiz97@gmail.com>
2023-12-06 14:34:42 -0800

    update css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import Logo from '../../assets/logo.png'
+import BarkLogo from '../../assets/BarkLogo.svg'
 import './Navbar.css'
 import { NavLink, Link } from 'react-router-dom';
 import { IoMdMenu, IoMdSettings } from "react-icons/io";
@@ -8,18 +8,18 @@ import { AnimatePresence, motion } from "framer-motion";
 const Navbar = () => {
     const [open, setOpen] = useState(false);
   return (
-    <header className='w-full shadow-[0_0_5px_0_rgba(0,0,0,0.3)] bg-white left-0 flex justify-center items-center fixed top-0 z-20 h-[75] px-5 py-1'>
+    <header className='w-full shadow-[0_0_5px_0_rgba(0,0,0,0.3)] bg-[#214038] left-0 flex justify-center items-center fixed top-0 z-20 h-[95px] px-5 py-1'>
         <div className="max-w-6xl w-full flex justify-between items-center ">
             <div className='flex gap-3 justify-center items-center'>
                 <NavLink to="/">
-                    <img src={Logo} className='w-auto h-20 py-2' alt="Logo" />
+                    <img src={BarkLogo} className='w-auto h-20 py-2' alt="Logo" />
                 </NavLink>
                 <div className='flex flex-col gap-1'>
-                    <h1 className='text-[1.79rem] max-lg:text-[1.5rem] leading-none'>BARK Animal Rescue</h1>
-                    <p className='text-sm leading-none'>No Kill Shelter in Alexandria, LA</p>
+                    <h1 className='text-[15px] font-[800] tracking-[.64px] font-[Poppins] text-[#E5BC01] leading-none'>BARK Animal Rescue</h1>
+                    <p className='text-[#F9F9F9] text-[11px] leading-none'>No Kill Shelter in Alexandria, LA</p>
                 </div>
             </div>
-            <div className='flex gap-5 text-black max-md:hidden'>
+            <div className='flex gap-5 text-[#F9F9F9] max-md:hidden'>
                 <NavLink to="/about">
                     About Bark
                 </NavLink>
@@ -32,11 +32,8 @@ const Navbar = () => {
                 <NavLink to="/resources">
                     Resources
                 </NavLink>
-                <NavLink to="/setting">
-                    Settings
-                </NavLink>
             </div>
-            <div className='text-2xl md:hidden cursor-pointer' onClick={() => setOpen(true)}>
+            <div className='text-2xl text-[#FFF] md:hidden cursor-pointer' onClick={() => setOpen(true)}>
                 <IoMdMenu />
             </div>
             <AnimatePresence>
@@ -52,35 +49,29 @@ export default Navbar
 const SideBarMenu = ({setOpen}) => {
     return (
         <motion.div initial={{ x: 100 }} animate={{ x: 0 }} exit={{ opacity: 0 }} onClick={() => setOpen(false)} className='bg-slate-900/20 backdrop-blur fixed inset-0 z-50 flex justify-end overflow-y-scroll cursor-pointer'>
-            <div onClick={(e) => e.stopPropagation()} className="bg-white h-full w-[90vw] relative z-10 cursor-default px-5 py-2 flex flex-col justify-between">
-                <div className='flex flex-col'>
-                    <div className='flex gap-3 justify-center items-center border-b border-gray-500 pb-5 mb-5'>
-                        <img src={Logo} className='w-auto h-20' alt="Logo" />
+            <div onClick={(e) => e.stopPropagation()} className="bg-[#7F3F98] h-full w-[90vw] relative z-10 cursor-default px-5 py-2 flex flex-col justify-between">
+                <div className='flex flex-col mt-8'>
+                    <div className='flex gap-3 justify-center items-center border-b border-[#F9F9F9] pb-5 mb-5'>
+                        <img src={BarkLogo} className='w-[50px] h-[50px]' alt="Logo" />
                         <div className='flex flex-col gap-1'>
-                            <h1 className='text-[1.79rem] max-lg:text-[1.5rem] leading-none'>BARK Animal Rescue</h1>
-                            <p className='text-sm leading-none'>Menu</p>
+                            <h1 className='text-[16px] text-[#F9F9F9] font-[600] font-[Poppins] leading-none'>BARK Animal Rescue</h1>
+                            <p className='text-[14px] text-[#F9F9F9] font-[Poppins] leading-none'>Menu</p>
                         </div>
                     </div>
-                    <div className='flex flex-col gap-5 '>
-                        <Link to="/about" className='border-b border-gray-500 pb-5' onClick={() => setOpen(false)}>
+                    <div className='flex flex-col gap-5 text-[#F9F9F9] font-[Poppins]'>
+                        <Link to="/about" className='border-b border-[#F9F9F9] pb-5' onClick={() => setOpen(false)}>
                             About BARK
                         </Link>
-                        <Link to="/adoption" className='border-b border-gray-500 pb-5' onClick={() => setOpen(false)}>
+                        <Link to="/adoption" className='border-b border-[#F9F9F9] pb-5' onClick={() => setOpen(false)}>
                             Adopt
                         </Link>
-                        <Link to="/donate" className='border-b border-gray-500 pb-5' onClick={() => setOpen(false)}>
+                        <Link to="/donate" className='border-b border-[#F9F9F9] pb-5' onClick={() => setOpen(false)}>
                             Donate
                         </Link>
-                        <Link to="/resources"  onClick={() => setOpen(false)}>
+                        <Link to="/resources" className='border-b border-[#F9F9F9] pb-5' onClick={() => setOpen(false)}>
                             Resources
                         </Link>
                     </div>
-                </div>
-                <div className='flex justify-center items-center  '>
-                    <Link to="/settings" className='flex justify-center p-3 items-center gap-3 cursor-pointer'  onClick={() => setOpen(false)}>
-                        <IoMdSettings />
-                        Settings
-                    </Link>
                 </div>
             </div>
         </motion.div>
