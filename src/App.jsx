084d755c9935df774843ae537c7afb562bf78051
jadewang425 <jadewang425@gmail.com>
2023-12-06 14:37:42 -0500

    add Donate page and update footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import AdoptionPage from './pages/AdoptionPage'
+import DonatePage from './pages/DonatePage'
 import PetDetailPage from './pages/PetDetailPage'
 // import { Link } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar'
@@ -21,6 +22,7 @@ function App() {
             path="/" 
             element={<HomePage/>}
           />
+          <Route path="/donate" element={<DonatePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/adoption" element={<AdoptionPage />} />
           <Route path='/adoption/:id' element={<PetDetailPage />} />
---
+++ src/pages/DonatePage.jsx
@@ -0,0 +1,5 @@
+export default function DonatePage () {
+    return (
+        <div>Donate Page</div>
+    )
+}
