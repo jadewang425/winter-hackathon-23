0ee4cb9da3375ad7af8bb6e3ffeeb74088205749
jadewang425 <jadewang425@gmail.com>
2023-12-06 14:29:50 -0500

    add mission statement

---
+++ src/components/Mission/Mission.jsx
@@ -0,0 +1,9 @@
+export default function Mission() {
+    return (
+        <div className="w-full h-auto bg-gray-300">
+            <div className="m-5">
+                Boudreaux’s Animal Rescue Krewe (B.A.R.K.) is a 501c(3) non-profit animal welfare organization and NO-KILL animal shelter in Alexandria.Louisiana.
+            </div>
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import AvailablePets from "../components/Navbar/AvailablePets"
 import Facebook from "../components/Facebook/Facebook"
 import FullCarousel from "../components/FullCarousel/FullCarousel"
+import Mission from "../components/Mission/Mission";
 
 const HomePage = () => {
   
   return (
     <div className="w-full flex flex-col justify-start items-center z-0 relative">
       <FullCarousel />
+      <Mission />
       <AvailablePets />
       <Facebook />
     </div>
