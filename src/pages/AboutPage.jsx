fb54aabb392dafe72c3c5b2d91b38f8aec512753
jadewang425 <jadewang425@gmail.com>
2023-12-07 13:20:07 -0500

    add images on the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import SectionHeader from "../components/SectionHeader"
+import img1 from '../assets/about_1.jpg'
+import img2 from '../assets/about_2.jpg'
+import img3 from '../assets/about_3.jpg'
 
 const AboutPage = () => {
 
@@ -48,23 +51,23 @@ const AboutPage = () => {
             <p className="font-bold text-[#121212] font-[Poppins] text-base mb-3">Boudreaux’s Animal Rescue Krewe (B.A.R.K.) is a 501c(3) non-profit animal welfare organization and NO-KILL animal shelter in Alexandria, Louisiana. </p>
             <p className="font-normal text-[#121212] font-[Poppins] text-base">We are determined and inspired to rescue homeless and/or unwanted dogs in our area because there are simply too many dogs that end up in shelters and get euthanized regularly. </p>
           </div>
-          <div className='flex justify-center items-center w-full h-[15rem] sm:w-full bg-slate-400 sm:bg-white'>
-            image placeholder
+          <div className='flex justify-center items-center w-full h-[15rem] sm:w-full'>
+            <img src={img1} alt="A Dog being examined" />
           </div>
           <p className="font-normal text-[#121212] font-[Poppins] text-base">
             Husband and wife team, Julian and Eva Long, have rescued animals all of their lives, but one day in 2004, when Julian was exposed to a local animal shelter’s day of euthanasia, he adopted ten dogs from “Death Row” and the two have
             never looked back. The tragedy of Hurricane Katrina hit one year later, and their shelter was overflowing with abandoned animals. Years have passed, and their incredible journey is on-going. Over 3,000 dogs have been rescued and placed
             in their forever homes, but there are always more in need
           </p>
-          <div className='flex justify-center items-center w-full h-[15rem] bg-slate-400 sm:bg-white sm:w-full '>
-            image placeholder
+          <div className='flex justify-center items-center w-full h-[15rem] sm:w-full'>
+            <img src={img2} alt="Dogs in crates on a lawn" />
           </div>
           <p className="font-normal text-[#121212] font-[Poppins] text-base">
             Both Julian and Eva were born and raised in Louisiana, and are very proud of their heritage. They love the southern traditions, the food, and the beautiful land, but regretfully admit that animals are not always treated as they should be.
             Sometimes they are treated like property and are kept on short chains. Some are mistreated and abused. Others run loose and are caught by animal control, keeping the shelters full of homeless pets.
           </p>
-          <div className='flex justify-center items-center w-full h-[15rem] bg-slate-400 sm:bg-white sm:w-full '>
-            image placeholder
+          <div className='flex justify-center items-center w-full h-[15rem] sm:w-full'>
+            <img src={img3} alt="A kitten being held" />
           </div>
           <p className="font-normal text-[#121212] font-[Poppins] text-base">
             <span className="font-semibold">The main message that that BARK would like to emphasize is the importance of spaying and neutering animals to help reduce animal overpopulation. </span>
@@ -77,8 +80,8 @@ const AboutPage = () => {
         </div>
       </div>
       <SectionHeader title='Find Us' />
-      <div className='max-w-6xl w-full flex flex-col justify-start gap-5 items-start sm:items-center sm:flex-row p-5 my-5'>
-        <div className="w-full self-center">
+      <div className='max-w-6xl w-full flex flex-col justify-start gap-5 items-start md:items-center md:flex-row p-5 my-5'>
+        <div className="w-full self-center md:w-[50%]">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109097.2177962001!2d-92.56474763701927!3d31.295782648889503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86254d49e9ffcce3%3A0xc1e65dd9e413f7ff!2sAlexandria%2C%20LA!5e0!3m2!1sen!2sus!4v1701933661147!5m2!1sen!2sus" width="100%" height="250" style={{ border: '0', borderRadius: '10px' }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div className='flex-1 flex flex-col gap-[16px]'>
