83110b54f7a101f95ed619272b8b142f697dd37f
mar69287 <marcoa.ruiz97@gmail.com>
2023-12-06 21:15:33 -0800

    finish adopt a pet section

--- src/pages/AdoptionPage.jsx
+++ src/pages/AdoptionPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { usePet } from "../utilities/PetContext";
 import { Link } from "react-router-dom";
 import SectionHeader from "../components/SectionHeader";
+import { BsDownload } from "react-icons/bs";
+import BarkLogo from '../assets/BarkLogo.svg'
 
 const AdoptionPage = () => {
   const { getPetByType } = usePet();
@@ -37,8 +39,8 @@ const AdoptionPage = () => {
         </div>
       </div>
       <SectionHeader title='Adopt a Pet' />
-      <div className="max-w-6xl  w-full flex flex-col gap-[20px] my-7 px-5">
-          <p className="">
+      <div className="max-w-3xl  w-full flex flex-col gap-[20px] my-7 px-5">
+          <p className="text-left sm:text-center">
             If you would like to adopt a pet, please fill out the applicable form below and send it to{' '}
             <a href="mailto:longj1003@aol.com">
               <span className='border-b border-gray-500 hover:border-black transition'>
@@ -47,7 +49,18 @@ const AdoptionPage = () => {
             </a>
             {' '}when complete.
           </p>
-          
+          <div className="flex flex-col min-[450px]:flex-row-reverse gap-10 justify-center items-center">
+            <div className="flex flex-col justify-center items-center gap-2 flex-1">
+              <img src={BarkLogo} className='w-auto h-32 py-2' alt="Logo" />
+              <p className="text-center">
+                Find your new best friend today!
+              </p>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-5 flex-1">
+              <button className="flex gap-5 justify-center items-center bg-[#E5BC01] rounded-full py-[6px] w-[240px]">Canine Application <BsDownload /></button>
+              <button className="flex gap-5 justify-center items-center bg-[#E5BC01] rounded-full py-[6px] w-[240px]">Feline Application <BsDownload /></button>
+            </div>
+          </div>
       </div>
     </div>
   );
