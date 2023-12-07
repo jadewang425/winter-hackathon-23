270311f5819396f8c932a0bbb52be1bb9006d704
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-12-07 13:59:34 -0500

    adjust styling on pet detail page

--- src/pages/PetDetailPage.jsx
+++ src/pages/PetDetailPage.jsx
@@ -49,33 +49,29 @@ const PetDetailPage = () => {
 
   return (
     <div className="w-full flex flex-col mt-8 justify-center items-center">
-      <div className="w-[200px] h-[250px] relative overflow-hidden">
+      <div className="w-[302px] h-[250px] relative overflow-hidden">
+        {/* placeholder for carousel images */}
         <img
           src={pet.photos.length > 0 && pet.photos[0].small ? pet.photos[0].small : placeholder}
           alt={pet.name}
           className="w-[100%] h-[100%] object-cover object-center rounded-xl"
         />
-        <div className="bg-white absolute w-full bottom-0 left-0 h-16 px-4 rounded-b-xl flex justify-center items-center tracking-[.018em] text-[#7F3F98]">
-          <h1 className="overflow-hidden whitespace-nowrap overflow-ellipsis">{pet.name}</h1>
-        </div>
-        <div className="bg-[#7F3F98] absolute  bottom-12 left-0 flex justify-center items-center tracking-[.018em] text-white">
-          <h1 className="py-2 px-3">Available</h1>
-        </div>
       </div>
-      <div className='text-center leading-8'>
-        <p>{pet.description}</p>
-        <p>Age: {pet.age}</p>
-        <p>Breed: {pet.breeds.primary}</p>
-        <p>Size: {pet.size}</p>
-        <p>Gender: {pet.gender}</p>
-        <p>Colors: {pet.colors.primary}</p>
-        <p>Attributes:</p>
+      <div className=' w-[302px] text-left leading-8 mt-10'>
+        <p> <strong>Name:</strong> {pet.name}</p>
+        <p> <strong>Age:</strong> {pet.age}</p>
+        <p> <strong>Breed:</strong> {pet.breeds.primary}</p>
+        <p> <strong>Size:</strong> {pet.size}</p>
+        <p> <strong>Gender:</strong> {pet.gender}</p>
+        <p> <strong>Background:</strong> {pet.description}</p>
+        <p> <strong>Color:</strong> {pet.colors && pet.colors.primary ? pet.colors.primary : 'Unknown'}</p>
+        <p> <strong>Attributes:</strong></p>
         <ul>
           {Object.entries(pet.attributes).map(([key, value]) => (
             <li key={key}> {renameValue(key, value)} </li>
           ))}
         </ul>
-        <p>Environment:</p>
+        <p> <strong>Environment:</strong></p>
         <ul>
           {Object.entries(pet.environment).map(([key, value]) => (
             <li key={key}> {renameValue(key, value)} </li>
