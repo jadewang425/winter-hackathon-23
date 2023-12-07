fcbafe25924b089caf2c3a3255e764602bc2fc4e
mar69287 <marcoa.ruiz97@gmail.com>
2023-12-06 20:03:19 -0800

    add new component

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,5 @@
+import SectionHeader from "../components/SectionHeader"
+
 const AboutPage = () => {
   return (
     <div className='max-w-[100%] flex flex-col items-center pt-5' >
@@ -81,13 +83,3 @@ const AboutPage = () => {
 }
 
 export default AboutPage
-
-const SectionHeader = ({title}) => {
-  return (
-    <div className='w-full p-4 bg-[#214038] flex justify-center items-start'>
-      <div className="max-w-6xl w-full">
-        <h1 className="text-base font-[Poppins] font-semibold text-[#F9F9F9]">{title}</h1>
-      </div>
-    </div>
-  )
-}
