12eb7ec984c4eca2435729749052ba760565b80e
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-12-11 13:00:19 -0500

    fix indentation issues on facebook component

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -5,19 +5,19 @@ export default function Facebook () {
     return (
         <div className="w-full max-w-2xl">
             <div className="py-5 px-4 sm:px-2">
-                 <div className="flex">
-                     <div className="pr-3 font-semibold  flex justify-center items-center">
-                         <p>Get to know us!</p>
-                     </div>
-                     <div className="self-center">
-                         <a href="https://www.facebook.com/barkanimalrescue"  target="_blank" rel="noreferrer">
-                             <FaFacebook size={25} />
-                         </a>
-                     </div>
-                 </div>
-                <div><p className="text-[14px]">Our recent posts</p></div>
-             </div>
+                <div className="flex">
+                    <div className="pr-3 font-semibold  flex justify-center items-center">
+                        <p>Get to know us!</p>
+                    </div>
+                    <div className="self-center">
+                        <a href="https://www.facebook.com/barkanimalrescue"  target="_blank" rel="noreferrer">
+                            <FaFacebook size={25} />
+                        </a>
 
+                    </div>
+                </div>
+                <div><p className="text-[14px]">Our recent posts</p></div>
+            </div>
             <div className="flex justify-center px-5">
                 <FacebookEmbed url="https://www.facebook.com/barkanimalrescue/posts/pfbid02CqS4KVLoyYmnuXfkGCFcs1PzLgcD3Ye5DA3pYjfpuU32RX1AdiPCNVYaDekFFkVJl" width={380} />
             </div>
